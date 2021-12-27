f0d4551f6c4e9c2e0e36ed6379d91c91e293cab1
Francisco Angel Romero Tepal <57637390+FranckRomer@users.noreply.github.com>
2021-12-27 11:50:26 -0600

    Add files via upload

---
+++ LA_TARJETA/LA_TARJETA.ino
@@ -0,0 +1,266 @@
+// SOCKET ESP32 con consulta/escritura en DB // VERSION SIN TRADUCIR
+// IP ESP32: 192.168.1.234
+
+#include <WiFi.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
+
+
+     // const char *ssid = "INFINITUM729C_2.4";
+     // const char *pass = "LaTarjeta123";
+      const char *ssid = "RED ACCESA";
+      const char *pass = "037E32E7";      
+      unsigned long t=0;
+
+   // Variables server
+      String texto, buf2; 
+      char buf[60],INSERT_SQL[200], evento[100];
+      int tam;
+
+  // Variables Pic
+      String comandoServer="", RESULT;
+      char buf_pic[50];
+
+      String  id_s;
+            
+      
+            WiFiServer server;       
+            WiFiClient client; 
+            MySQL_Connection conn((Client*)&client);
+            MySQL_Cursor* cursor;
+                  
+      
+      
+            IPAddress server_addr(192,168,1,221);  // IP del server MySQL:  231 - 221
+            char user[] = "root";              // MySQL usuario
+            char password[] = "ALAala123,.-";        // Contraseña MySQL        
+
+                  
+                  ////Obtener la hora/////
+                 String get_time(){
+                      time_t now;
+                      time(&now);
+                      char time_output[30];
+                      strftime(time_output, 25, "%d/%m/%y %T", localtime(&now)); 
+                      return String(time_output); // Formato:   20/Sep/21 12:30:45
+                      }
+                String HOUR;
+ 
+  void setup()
+          {
+              Serial.begin(115200);
+              Serial.println();
+              t=millis();
+          
+              WiFi.mode(WIFI_STA);              
+              WiFi.begin(ssid, pass);
+              configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+              setenv("TZ", "CST6CDT,M4.1.0,M10.5.0", 1);
+              
+              while (WiFi.status() != WL_CONNECTED)
+                {
+                  delay(100);
+                  Serial.print(".");
+                  }
+              Serial.println("Conectado a red WiFi");
+              Serial.print("Direccion IP del ESP32 : ");
+              Serial.println(WiFi.localIP());         
+              server.begin(132); // El servidor comienza a escuchar el puerto número 132 
+
+              delay(500);
+
+              Serial.print("Connecting to SQL...  ");
+               if (conn.connect(server_addr,3306, user, password))
+                    Serial.println("SQL OK.");
+               else
+                    Serial.println("SQL FAILED.");
+
+                  //create MySQL cursor object
+              cursor = new MySQL_Cursor(&conn);  
+ 
+            delay(1000);
+              
+          } //Ciero el void setup
+
+
+  void loop()
+              {   
+
+              WiFiClient client = server.available(); // Intenta crear un objeto de cliente
+              if (client) // Si el cliente está disponible
+              {
+               Serial.println("[Cliente conectado]");
+               
+                  while (client.connected()) // Si el cliente está conectado
+                  {
+
+                                while (millis()-t>=1500)
+                                {                                
+                                client.print("!");  // Envío heartbeat
+                                
+                                                             
+                                TimerServerSQLRead (); //}// Timer para leer DB cada segundo
+                                        
+                                        //RESULT.replace("<","<TPV");
+                                        client.println(RESULT);
+                                          
+                                         
+                                           if (RESULT !=0)
+                                          {              
+                                            Serial.println(RESULT);
+                                            MySQL_Cursor *cur_update = new MySQL_Cursor(&conn);
+                                            //String update_tabla = "UPDATE conacclatarjeta.eventosPC SET fechaAtencion = '" + get_time() + "', estatus = 'leido' WHERE id = " + id_s +";";
+                                            String update_tabla = "UPDATE conacclatarjeta.eventosPC SET fechaAtencion = '" + get_time() + "', estatus = 'leido' WHERE id = " + id_s +";";                                            
+                                            char cuery_UP [200];
+                                            update_tabla.toCharArray(cuery_UP,200);
+                                            cur_update->execute(cuery_UP);
+
+//                                              if (result.indexOf("RST")==4 || result.indexOf("RST")==7 )
+//                                              {                                               
+//                                              ESP.restart(); 
+//                                              }
+                                              
+                                              Serial.println("DB: Evento actualizado ");
+                                              
+                                          }
+                                          else if (RESULT==""){
+                                            ///
+                                
+                                          }    
+                                                RESULT="";
+                                                    
+                                       //////////***********///////////**********///////////********
+
+                                t=millis();  
+                                   }
+                                        
+                                if (client.available()) {
+                            
+                                    tam= client.readBytesUntil('\n',buf, 60);  //Para leer el numero de bytes
+                                    buf[tam]='\0';                             
+                                    texto = String(buf);   //String
+                                    
+                                           
+//////////////////////////////////////////////////////   PROCESO   /////////////////////////////////////////////////////////////////
+                           
+                           if (texto.indexOf("TPV")>=1)
+                           {
+
+                                ///////////
+                                
+                                String bufTPV;
+                                char INSERT_SQL_TPV[200];
+                                texto.replace("<>", "");
+                                
+                                //bufTPV = "INSERT INTO conacclatarjeta.eventosfrompic(fechaRegistro, evento, estatus) VALUES ( '"+ get_time() +"','"+texto+"', 'sin leer')";          
+                                bufTPV = "INSERT INTO conacclatarjeta.eventosfrompic(fechaRegistro, evento, estatus) VALUES ( '"+ get_time() +"','"+texto+"', 'sin leer')";          
+                                bufTPV.toCharArray(INSERT_SQL_TPV,200);
+                                Serial.println(bufTPV);
+                                
+                                if (conn.connected()){
+                                cursor->execute(INSERT_SQL_TPV);}                                                              
+                                
+                                //texto.replace("TPV", "");                                 
+                                //Serial.println(texto);
+                                       
+//                                ParcheToS();
+
+                                buf2 = "INSERT INTO conacclatarjeta.eventospic(fechaRegistro, fechaAtencion, evento, estatus) VALUES ( '"+ get_time() +"', '' ,'"+texto+"', 'sin leer')";          
+                                buf2.toCharArray(INSERT_SQL,200);
+                                Serial.println(INSERT_SQL);   
+                                
+                                if (conn.connected()){
+                                cursor->execute(INSERT_SQL);
+                                     }
+
+                                client.print("<SQLWRT1>");
+                                delay(200);                                                                      
+                                                            
+                           }
+
+
+//////////////////////////////////////////                                   
+                                                   
+                                 } // Se cierra el client.available 
+                          } // Se cierra el client.connected
+                                                                                
+                // Si el cliente se desconecta 
+                  client.stop();
+                  Serial.println();
+                  Serial.println("[Cliente desconectado]");
+
+                     } //Se cierra el client
+                  } // Se cierra el Loop
+
+   /// AQUI TERMINA EL PROGRAMA PRINCIPAL 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+// FUNCIONES :   
+  void TimerServerSQLRead ()  
+       {
+                         
+                          row_values *row = NULL;
+                          long head_count=0, head_countEv = 0;
+                          id_s="";
+                          MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);    
+                                                            
+                          char id[80]="SELECT id FROM conacclatarjeta.eventosPC WHERE estatus = 'sin leer';"; //query                           
+                          cur_mem->execute(id);
+
+                     ////////////////////////////////////////////////////////////////  
+                                  column_names *cols = cur_mem->get_columns();
+                                  do {
+                                   row = cur_mem->get_next_row();
+                                   if (row != NULL) {
+                                   head_count = atol(row->values[0]);
+                                   }
+                                   } while (row != NULL);
+                                    //Deleting the cursor also frees up memory used
+                                   delete cur_mem;
+                     ////////////////////////////////////////////////////////////////
+
+                                 int id_i;
+ 
+                                  id_s= String(head_count);
+                                  id_i= id_s.toInt();
+
+
+                            if (id_i > 0 )
+                            {
+                               row_values *rowID = NULL;
+                               long head_countEv = 0;
+                               MySQL_Cursor *cur_memID = new MySQL_Cursor(&conn);
+                               String row_ID;
+                                
+                               String evento_string= "SELECT evento FROM conacclatarjeta.eventosPC WHERE estatus = 'sin leer' AND id = '"+id_s+"';";
+                               evento_string.toCharArray(evento,100);
+                               cur_memID->execute(evento);
+                                  
+                     ////////////////////////////////////////////////////////////////
+
+                                  column_names *colsID = cur_memID->get_columns();                                  
+                                  do {
+                                   rowID = cur_memID->get_next_row();
+                                   if (rowID != NULL) {
+                                   row_ID= rowID->values[0]; 
+                                   }
+                                   } while (rowID != NULL);
+                                   delete cur_memID;
+                     ////////////////////////////////////////////////////////////////
+
+                                   row_ID.toCharArray(buf_pic, 50) ; //<-- BUF_PIC : Guarda el valor del evento en Char (Ex. RESET_TPV )
+                                   RESULT = row_ID; 
+              
+
+                              }
+
+                
+                } // Se cierra el TimerServerSQLRead
